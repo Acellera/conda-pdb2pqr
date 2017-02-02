a12225d2e51ae00bb795cb0d7320e240e7266301
Toni G <toni.giorgino@gmail.com>
2017-02-02 12:23:36 +0100

    auto trigger

---
+++ release.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [[ $# -ne 1 ]]; then
+    echo Latest tag is `git describe --tags`
+    exit 0
+fi
+
+tag=$1
+
+date > trigger_file
+git commit -m "Pre-tag commit for $tag"
+git tag $tag
+date > trigger_file
+git commit -m "Post-tag commit for $tag to trigger rebuild"
+git push
+
